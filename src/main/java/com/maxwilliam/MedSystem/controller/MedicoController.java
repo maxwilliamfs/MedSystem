0b6a7484ff92d456b792745012867470d8041c4b
MegaMex <williammaxbcc4794@gmail.com>
2026-03-19 22:03:51 -0300

    Funcionario UPDATE
    Adicionei a parte de seguranca no funcionario

--- src/main/java/com/maxwilliam/MedSystem/controller/MedicoController.java
+++ src/main/java/com/maxwilliam/MedSystem/controller/MedicoController.java
@@ -1,55 +1,41 @@
 package com.maxwilliam.MedSystem.controller;
 
-<<<<<<< Updated upstream
 import com.maxwilliam.MedSystem.dto.RealizarConsultaDTO;
 import com.maxwilliam.MedSystem.exception.MedSystemException;
 import com.maxwilliam.MedSystem.model.ConsultaAbstrata;
 import com.maxwilliam.MedSystem.model.Medico;
 import com.maxwilliam.MedSystem.model.secundarias.Data;
 import com.maxwilliam.MedSystem.service.ServiceConsulta;
-import jakarta.validation.Valid;
 import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
-=======
-import com.maxwilliam.MedSystem.exception.MedSystemException;
-import com.maxwilliam.MedSystem.model.ConsultaAbstrata;
-import com.maxwilliam.MedSystem.model.Medico;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> Stashed changes
 
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/medicos")
-public class MedicoController extends BaseFuncionarioController<Medico>{
-<<<<<<< Updated upstream
+public class MedicoController extends BaseFuncionarioController<Medico> {
+
     @Autowired
     ServiceConsulta serviceConsulta;
 
     @GetMapping("/{cpf}/{dia}/{mes}/{ano}/agenda")
     public ArrayList<ConsultaAbstrata> verAgenda(@PathVariable
     @CPF(message = "Informe um CPF valido") String cpf, @PathVariable int dia,
-    @PathVariable int mes, @PathVariable int ano) throws MedSystemException{
-        Data d = new Data(dia,mes,ano);
-        return serviceConsulta.agendaMedica(cpf,d);
+    @PathVariable int mes, @PathVariable int ano) throws MedSystemException {
+        Data d = new Data(dia, mes, ano);
+        return serviceConsulta.agendaMedica(cpf, d);
     }
 
     @PutMapping("/realizarConsulta")
-    public void realizarConsulta (@RequestBody RealizarConsultaDTO dados) throws
-    MedSystemException{
-        serviceConsulta.realizarConsulta(dados.getId(),dados.getProcedimentos()
-        ,dados.getPrescricaoMedica());
-=======
-    @GetMapping("/{cpf}/agenda")
-    public ArrayList<ConsultaAbstrata> verAgenda(@PathVariable String cpf)
-    throws MedSystemException{
-        return serviceFuncionario;
->>>>>>> Stashed changes
+    public void realizarConsulta(@RequestBody RealizarConsultaDTO dados) throws
+            MedSystemException {
+        serviceConsulta.realizarConsulta(dados.getId(), dados.getProcedimentos()
+                , dados.getPrescricaoMedica());
     }
 }
+
--- src/main/java/com/maxwilliam/MedSystem/model/Funcionario.java
+++ src/main/java/com/maxwilliam/MedSystem/model/Funcionario.java
@@ -4,9 +4,16 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.maxwilliam.MedSystem.model.secundarias.Data;
 import com.maxwilliam.MedSystem.model.secundarias.Endereco;
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 
-public class Funcionario extends Pessoa {
+public class Funcionario extends Pessoa implements UserDetails {
     @NotBlank(message = "Informe uma senha")
     private String senha;
 
@@ -31,7 +38,37 @@ public class Funcionario extends Pessoa {
     @JsonCreator
     public Funcionario(){
         super();
-    } 
+    }
+
+    //Metodos
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+    @Override
+    public String getPassword(){
+        return this.getSenha();
+    }
+    @Override
+    public String getUsername(){
+        return this.getCPF();
+    }
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+    @Override
+    public boolean isEnabled(){
+        return true;
+    }
 
     //Getters e Setters
     public String getSenha() {
